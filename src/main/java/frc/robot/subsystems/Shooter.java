57b8768bc2e2a9aa9d4e0cce8919811433d25ca2
rohandurgumcoughcough <rohan.durgum@pbstudents.com>
2021-01-26 18:28:28 -0600

    help2

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
   private CANSparkMax shoot;
