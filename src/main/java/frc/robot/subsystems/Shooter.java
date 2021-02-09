19880e39555e0593de0f35a4f2c01f70aa169985
Bots <Bulah.Bots@parkviewbaptist.com>
2021-02-09 16:51:48 -0600

    recommit for Pickup and Commands

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,11 +3,14 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Servo;
 
 public class Shooter extends SubsystemBase {
   private CANSparkMax shoot;
+  private Servo bigBoys;
 
-  public Shooter() {
+  public Shooter(int shooterMotor, int bigBoysPort) {
+    shooterMotor = new CANSparkMax(shooterMotor, MotorType.kBrushless);
 
   }
 
