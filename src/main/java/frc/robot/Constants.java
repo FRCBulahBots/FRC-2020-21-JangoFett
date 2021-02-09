de6be71a502923746d7c2236a2370278f17acf97
Bots <Bulah.Bots@parkviewbaptist.com>
2021-02-09 16:18:52 -0600

    added Pickup and commands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,19 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
     public static int leftMasterChief = 0;
@@ -22,4 +28,28 @@ public final class Constants {
     public static int rightFollower = 3;
 
     public static int joystick = 0;
+    public static class ArmManipulator {   
+        public WPI_TalonSRX armMotor;
+        public Encoder armEncoder;
+        public double kP = 0.002f; // Proportional controller constant
+        public double kI = 0.0f; // Integral controller constant
+        public int TargetPosition = 0; // Target Positional value of Arm (Defual target is 0;)
+
+        public ArmManipulator(int armManipulatorMotor, int encA, int encB){
+            armMotor = new WPI_TalonSRX(armManipulatorMotor);
+            armEncoder = new Encoder(encA, encB, true, Encoder.EncodingType.k4X);
+            armEncoder.setDistancePerPulse(7);
+        }
+
+        
+        public enum ArmPositions { // Enum containing all of the possible Arm Postions we may want to move to
+            FULLYRETRACTED(0),
+            BALLPICKUP(1404);
+    
+            private final int id;
+            ArmPositions(int id) { this.id = id; }
+            public int getValue() { return id; }
+        }
+    }
+    
 }
